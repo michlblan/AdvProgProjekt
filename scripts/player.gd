a94c736104d3bc2f378a0e6d17e9f3305bdc8d97
Michal Blanař <m_blanar@utb.cz>
2026-04-29 10:13:34 +0200

    Připojení pohybu na klávesy & otáčení postavy podle směru pohybu

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+@onready var playerSprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +10,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	print(direction)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if (direction == -1):
+		playerSprite.flip_h = true
+
+	if (direction == 1):
+		playerSprite.flip_h = false
 
 	move_and_slide()
