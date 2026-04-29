e03b64ebf4aa56af93e7eca9fb208ea546da0153
Michal Blanař <m_blanar@utb.cz>
2026-04-29 09:47:41 +0200

    Improve death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,10 +4,13 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
+	body.get_node("CollisionShape2D").queue_free()
 	print("Youre dead")
+	Engine.time_scale = 0.5
 	timer.start()
 
 
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1
