b151b00f725b73c5f8fe0f93937e924d0bd7baf7
Michal Blanař <m_blanar@utb.cz>
2026-05-10 01:20:53 +0200

    Přidáno sbírání mincí se zvukem a zvuk pozadí

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @onready var gameManager = %GameManager
+@onready var animationPlayer = $AnimationPlayer
 
 func _on_body_entered(_body: Node2D) -> void:
 	gameManager.addPoint()
-	queue_free()
+	animationPlayer.play("PickupAnimation")
