b3e6e1e7d850b976e782cafc8805e3bae1493023
Michal Blanař <m_blanar@utb.cz>
2026-05-15 02:28:08 +0200

    Přidán shitty Sprite ze hry co sem dělal v 16ti
    Je až komické jak nevystředěné to je ale in my defense
    v tom enginu co jsem používal jsem ty snímky skládal manuálně
    takže to takový problém nebyl

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -280.0
 @onready var playerSprite = $AnimatedSprite2D
+
+func playIfNotPlaying(player :AnimatedSprite2D, animation :String):
+	"""Plays character animation if it's not playing yet"""
+	if (player.animation != animation):
+		player.play(animation)
 
 func _physics_process(delta: float) -> void:
 	var isOnFloor = is_on_floor()
@@ -11,12 +16,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		playerSprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	# Chtělo by to předělat to přehrávání animací
-	# Za chvíly to bude hrozný clusterfuck if a if not
 	if direction:
 		velocity.x = direction * SPEED
 		if isOnFloor:
@@ -27,16 +31,15 @@ func _physics_process(delta: float) -> void:
 			playerSprite.play("idle")
 
 	if (direction == -1):
-		playerSprite.flip_h = true
-
-	if (direction == 1):
 		playerSprite.flip_h = false
 
-	if not isOnFloor:
-		velocity += get_gravity() * delta
-		print(playerSprite.animation)
-		if (playerSprite.animation != "jump"):
-			print("not jumping jet")
-			playerSprite.play("jump")
+	if (direction == 1):
+		playerSprite.flip_h = true
 
+	if not isOnFloor:
+		# Tento řádek nás posunuje k zemi
+		velocity += (get_gravity()*0.8) * delta
+		playIfNotPlaying(playerSprite, "jump" if (velocity.y < 0) else "fall")
+
+	# print("Leaving fun with: " + playerSprite.animation)
 	move_and_slide()
