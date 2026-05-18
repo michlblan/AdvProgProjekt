1568af176c4e838e351ee4aeb41b80b9fda65cdf
Michal Blanař <m_blanar@utb.cz>
2026-05-19 01:53:45 +0200

    Přidána aktualizace HPček a smrt při 0

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,10 +1,19 @@
 extends Node
 
 @onready var cashLabel = $"Canvas/CashLabel"
+var hearts: Array
 
 var score = 0
+
+func _ready():
+	hearts = $Canvas.find_children("Srdce*", "Sprite2D")
 
 func addPoint():
 	score +=1
 	cashLabel.text = "Nasbíráno kováků: %s" % score
 	print(score)
+
+func updateHp(newLives :int):
+	print("Nově máme %s životů" % newLives)
+	for i in range(len(hearts)):
+		hearts[i].visible = !(newLives <= i)
