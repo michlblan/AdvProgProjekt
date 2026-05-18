6ed2bad6b8253941f7b5a1e734dcc339455e46e5
Michal Blanař <m_blanar@utb.cz>
2026-05-19 01:26:52 +0200

    Finally, clean implementation of damage intake
    Still probably retarded as fuck but now it works and it will fucking work

---
+++ scripts/game.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var player = $player
+@onready var enemy = $Slime
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	enemy.playerHit.connect(player.takeDamage)
+	print("Connected")
