f6d0636ac0c44bdea1040ba2f4683038a5d20ed5
Michal Blanař <m_blanar@utb.cz>
2026-05-20 00:25:40 +0200

    Make all enemies give damage

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 @onready var player = $player
-@onready var enemy = $Slime
+var enemies :Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	enemy.playerHit.connect(player.takeDamage)
-	print("Connected")
+	enemies = $Enemies.find_children("Slime*", "Area2D")
+
+	for e in enemies:
+		e.playerHit.connect(player.takeDamage)
+	print("Connected %s enemies!" % len(enemies))
